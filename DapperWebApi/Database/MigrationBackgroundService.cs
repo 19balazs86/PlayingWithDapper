1182c569fbac727b25eab8644f279c655b479efb
19balazs86 <19balazs86@gmail.com>
2024-10-29 10:27:16 +0100

    Add an example using the NpgsqlBinaryImporter

--- DapperWebApi/Database/MigrationBackgroundService.cs
+++ DapperWebApi/Database/MigrationBackgroundService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using DbUp;
 using DbUp.Engine;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace DapperWebApi.Database;
 
@@ -11,6 +13,8 @@ public sealed class MigrationBackgroundService(
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         await Task.Run(() => DatabaseMigration.Run(_connectionStringProvider.ConnectionString, _logger), stoppingToken);
+
+        // await DatabaseMigration.ExampleBinaryImport(_connectionStringProvider.ConnectionString);
     }
 }
 
@@ -45,5 +49,30 @@ public static class DatabaseMigration
         {
             logger.LogError(ex, "Migration failed to run");
         }
+    }
+
+    public static async Task ExampleBinaryImport(string connectionString)
+    {
+        const string sql = "COPY bookings (room_id, start_date, end_date, total_price) FROM STDIN (FORMAT BINARY)";
+
+        await using var connection = new NpgsqlConnection(connectionString);
+
+        await connection.OpenAsync();
+
+        // High-performance bulk data import of binary data directly into Postgres
+        // Bulk inserts using binary format are significantly faster compared text-based bulk loading
+        await using NpgsqlBinaryImporter binaryImporter = await connection.BeginBinaryImportAsync(sql);
+
+        for (int roomId = 1; roomId <= 10; roomId++)
+        {
+            await binaryImporter.StartRowAsync();
+
+            await binaryImporter.WriteAsync(roomId,                          NpgsqlDbType.Integer);
+            await binaryImporter.WriteAsync(DateTime.UtcNow.Date,            NpgsqlDbType.Date);
+            await binaryImporter.WriteAsync(DateTime.UtcNow.Date.AddDays(5), NpgsqlDbType.Date);
+            await binaryImporter.WriteAsync(123.45,                          NpgsqlDbType.Numeric);
+        }
+
+        await binaryImporter.CompleteAsync();
     }
 }
