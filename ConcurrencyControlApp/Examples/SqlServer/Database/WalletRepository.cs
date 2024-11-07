f3afc548fd698099d3ae7a5283923513e706dba9
19balazs86 <19balazs86@gmail.com>
2024-11-07 09:52:08 +0100

    New example for the usage of RowVersion in a new method TransferMoneyBetweenWalletsV2

--- ConcurrencyControlApp/Examples/SqlServer/Database/WalletRepository.cs
+++ ConcurrencyControlApp/Examples/SqlServer/Database/WalletRepository.cs
@@ -10,6 +10,8 @@ public interface IWalletRepository
 
     public Task<bool> Update(Wallet wallet);
 
+    public Task<bool> TransferMoneyBetweenWallets(Wallet fromWallet, Wallet toWallet);
+
     public Task EnsureTableCreated();
 }
 
@@ -45,6 +47,46 @@ public sealed class WalletRepository(IDbConnectionManager _dbConnection) : IWall
         int numberOfRowsAffected = await connection.ExecuteAsync(sql, param: wallet, transaction: _dbConnection.Transaction);
 
         return numberOfRowsAffected == 1;
+    }
+
+    public async Task<bool> TransferMoneyBetweenWallets(Wallet fromWallet, Wallet toWallet)
+    {
+        // Better solution is a combination of a User-Defined Table Type and a stored procedure
+        // All in all, there are multiple ways to perform this transfer, but the focus is on using RowVersion
+        const string sql =
+            """
+            -- Declare a table variable to hold wallets values to update
+            DECLARE @WalletTempTable TABLE (Id INT, Balance DECIMAL(18, 2), RowVersion BINARY(8));
+            
+            -- Insert values to the temporary table
+            INSERT INTO @WalletTempTable (Id, Balance, RowVersion)
+            VALUES 
+                (@fromId, @fromBalance, @fromRowVersion),
+                (@toId,   @toBalance,   @toRowVersion);
+            
+            -- Update the Wallets table using the JOIN and READPAST hint
+            UPDATE target
+                SET target.Balance = wtt.Balance
+            FROM [Wallets] target WITH (READPAST) -- Apply the READPAST hint
+            INNER JOIN @WalletTempTable wtt 
+                ON target.Id = wtt.Id AND target.RowVersion = wtt.RowVersion;
+            """;
+
+        var param = new
+        {
+            fromId         = fromWallet.Id,
+            fromBalance    = fromWallet.Balance,
+            fromRowVersion = fromWallet.RowVersion,
+            toId           = toWallet.Id,
+            toBalance      = toWallet.Balance,
+            toRowVersion   = toWallet.RowVersion
+        };
+
+        DbConnection connection = await _dbConnection.OpenConnection();
+
+        int numberOfRowsAffected = await connection.ExecuteAsync(sql, param, transaction: _dbConnection.Transaction);
+
+        return numberOfRowsAffected == 4; // The expected number is 4 because there are 2 inserts and 2 updates
     }
 
     public async Task EnsureTableCreated()
