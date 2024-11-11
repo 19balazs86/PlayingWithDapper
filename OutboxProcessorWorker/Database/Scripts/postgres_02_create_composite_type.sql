badfe0b7eff821d665891d9cb48b2d5f56b374ee
19balazs86 <19balazs86@gmail.com>
2024-11-11 14:37:46 +0100

    Remove a comment from SQL script.

--- OutboxProcessorWorker/Database/Scripts/postgres_02_create_composite_type.sql
+++ OutboxProcessorWorker/Database/Scripts/postgres_02_create_composite_type.sql
@@ -3,5 +3,3 @@ CREATE TYPE outbox_update_type AS (
     id UUID,
     error TEXT
 );
-
--- Eventually, this type is not used, because I could not get it to work when calling a stored procedure with a composite type in Postgres using Dapper
