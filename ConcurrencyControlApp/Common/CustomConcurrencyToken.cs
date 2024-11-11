6b424121d338d581199f32f19903d0a640936789
19balazs86 <19balazs86@gmail.com>
2024-11-11 17:22:36 +0100

    Add examples configuring ConcurrencyToken

--- ConcurrencyControlApp/Common/CustomConcurrencyToken.cs
+++ ConcurrencyControlApp/Common/CustomConcurrencyToken.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ConcurrencyControlApp.Common;
 
 // The following example demonstrates how to create a unified solution for SQL Server and Postgres to manage and handle the concurrency token
 // Handling concurrency conflicts with EF: https://youtu.be/cEQxX1yM-Zc
+
 public abstract class EntityBase
 {
     public Guid Id { get; set; }
@@ -20,9 +23,16 @@ public sealed class ExampleDbContext(DbContextOptions _options) : DbContext(_opt
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        // #1 - The ConcurrencyToken configuration can be applied individually to each entity
         builder.Entity<ExampleEntity>()
                .Property(x => x.Version)
                .IsConcurrencyToken();
+
+        // #2 - Through IEntityTypeConfiguration
+        // builder.ApplyConfiguration(new ExampleEntityTypeConfiguration());
+
+        // #3 - Or by iterating over the entities with a method call
+        // setConcurrencyTokenForEntityBase(builder);
 
         base.OnModelCreating(builder);
     }
@@ -39,5 +49,38 @@ public sealed class ExampleDbContext(DbContextOptions _options) : DbContext(_opt
         }
 
         return base.SaveChangesAsync(ct);
+    }
+
+    private static void setConcurrencyTokenForEntityBase(ModelBuilder modelBuilder)
+    {
+        foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            // if (typeof(EntityBase).IsAssignableFrom(entityType.ClrType))
+
+            IMutableProperty? versionProperty = entityType.FindProperty(nameof(EntityBase.Version));
+
+            if (versionProperty is null) continue;
+
+            versionProperty.IsConcurrencyToken = true;
+        }
+    }
+}
+
+public sealed class ExampleEntityTypeConfiguration : EntityBaseTypeConfiguration<ExampleEntity>
+{
+    public override void Configure(EntityTypeBuilder<ExampleEntity> builder)
+    {
+        // Add configurations related to the entity
+
+        base.Configure(builder);
+    }
+}
+
+public abstract class EntityBaseTypeConfiguration<TEntityBase> : IEntityTypeConfiguration<TEntityBase> where TEntityBase : EntityBase
+{
+    public virtual void Configure(EntityTypeBuilder<TEntityBase> builder)
+    {
+        builder.Property(p => p.Version)
+               .IsConcurrencyToken();
     }
 }
